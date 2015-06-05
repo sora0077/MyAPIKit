f09e1a3f1abb7860085f4642ef321cd142ae187b
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-06 02:54:09 +0900

    Initial Commit

---
+++ APIKit/APIKit.h
@@ -0,0 +1,19 @@
+//
+//  APIKit.h
+//  APIKit
+//
+//  Created by 林達也 on 2015/06/06.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for APIKit.
+FOUNDATION_EXPORT double APIKitVersionNumber;
+
+//! Project version string for APIKit.
+FOUNDATION_EXPORT const unsigned char APIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <APIKit/PublicHeader.h>
+
+
